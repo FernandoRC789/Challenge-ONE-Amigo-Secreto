4c4106d8beb9b794e375df8491225fec4b90a27a
FernandoRC789 <nickfernando789@gmail.com>
2025-02-10 18:44:10 -0500

    proyecto terminado
    se hicieron modificaiones en el codigo y comentario se probo el programa y se  corrigieron alguno error que se presentaron, quedo bien el juego, se realizo de acuerdo a los requerimientos de l archivo de trello que proporciono alura latam

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
 /*Declarando una variable de tipo array, que almacenará los nombres de los amigos ingresados.*/
-
 let amigosList = [];
-let contador = 1;
 
 /*Función que permita al usuario ingresar un nombre en el campo de texto 
 y añadirlo a la lista de amigos creada anteriormente.*/
 function agregarAmigo() {
     let amigoRegistrado = document.getElementById("amigo").value.trim();  
  // Verificar si el campo no está vacío la primera vez 
-    if (contador != 1) {
-        if ( amigoRegistrado != '') { //Verificar si el campo no está vacío
+    if ( amigoRegistrado != '') { //Verificar si el campo no está vacío
+        if (/^[A-Za-zñÑ\s]+$/.test(amigoRegistrado)) {
             amigosList.push(amigoRegistrado);  // Agregar el amigo al array
         
             // Mostrar la lista de amigos en la página es una función
             actualizarListaDeAmigos();
-        
+    
             // Limpiar el campo de texto
             document.getElementById("amigo").value = '';
-        
-            // Incrementar el contador
-            contador++;
-        } else {
-            alert("Por favor, ingresa un nombre.");
+        }else{
+            alert('Ingrese solo Texto');
+            document.getElementById("amigo").value = '';
         }
-    }else{
-        contador++;
+    } else {
+        alert("Por favor, ingresa un nombre.");
     }
 }
 
@@ -42,51 +38,30 @@ function actualizarListaDeAmigos() {
 
 //Función para obtener un número aleatorio
 function numeroAleatorio(){
-    let numeroRandom = Math.floor(Math.random() * 10);
-    return numeroRandom;
+    let numeroSecreto = Math.floor(Math.random() * amigosList.length);
+    return numeroSecreto;
 }
+
 
 
 function sortearAmigo(){
     let numeroSecreto = numeroAleatorio();
-    console.log('el numero secreto es : ' + numeroSecreto);
-    console.log("el numero random es " + numeroSecreto)
-    
+
     if ( amigosList.length !== 0) { //Verificar si el campo no está vacío
-        alert("la lista esta completa");
-        contador++;
-    } else {
-        alert("la lista esta vacia");
-    }
-
-}
-
-
-    /*if (amigosList.length != 0 && contador != 1) {
         for (let i = 0; i < amigosList.length; i++) {
-            if (i === numeroSecreto) {
-                console.log(i);
-                console.log(amigosList[i] === numeroSecreto);
-                console.log(amigosList[i]);
-                console.log('el numero secreto if es:'+numeroSecreto)
-                console.log("el amigo secreto es :" + amigosList[i])
-                break;
-            }else{
-                console.log(amigosList[i] === numeroSecreto);
-                console.log(amigosList[i]);
-                console.log(i);
-                console.log('else el numero secreto if es:'+numeroSecreto)
-                console.log("else el amigo secreto es :" + amigosList[i])
-                console.log("no se encontro ningun amigo secreto");
-                alert("ingrese nombres a sortearrrrr");
+            if (i === numeroSecreto ) {
+                let resultado = document.getElementById('resultado');
+                resultado.innerHTML = `El amigo secreto sorteado es: ${amigosList[i]}`;
+                limpiarCampos('listaAmigos');
             }
         }
-    }else{
-        alert("ingrese nombres a sortearrrrr if amigos ");
-        //return;
-        contador++;
-    }*/
+    } else {
+        alert("No hay Nombres para Sortear");
+    }
+}
 
-
-//numeroAleatorio();
-//sortearAmigo();
+//limpiar campos con el id como parametro de la función
+function limpiarCampos(texto){
+    let textoEliminar = document.getElementById(texto);
+    textoEliminar.innerHTML = '';
+}
